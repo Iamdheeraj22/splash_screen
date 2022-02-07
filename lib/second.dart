09699eb1b2fbd58d4c235eac5d11f9ab068b01a8
Dheeraj Prajapat <dheerajprajapati334@gmail.com>
2022-02-07 23:57:23 +0530

    Today Prctice

--- lib/second.dart
+++ lib/second.dart
@@ -11,10 +11,27 @@ class _SecondPageState extends State<SecondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Second activity"),
-      ),
-      body: const Center(child: Text("Hello World")),
-    );
+        appBar: AppBar(
+          title: const Text("Second activity"),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text("Hii"),
+              Text("Bye"),
+              Text("Good Night"),
+              Text("Good evening"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text("Text Button 1"),
+                  Text("Text Button 2"),
+                  Text("Text Button 3"),
+                  Text("Text Button 4")
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
